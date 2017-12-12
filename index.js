c69ccd13a09d636054cd74aafda63eef20b77cba
Ryan Farney <farneyr@yahoo.com>
2017-12-12 22:27:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,9 +18,9 @@ function sayHiToGrandma(string) {
   if (string === 'I love you, Grandma.') {
     return "I love you, too.";
   } else if (string !== string.toUpperCase()) {
-    return "I can't hear you!"
+    return "I can't hear you!";
   } else {
-    return 'YES INDEED!'
+    return 'YES INDEED!';
   }
 }
 
