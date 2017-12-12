3718f51158a06dc9738f49bc718b12303a8fa868
Ryan Farney <farneyr@yahoo.com>
2017-12-12 22:24:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -15,14 +15,13 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string !== string.toUpperCase()) {
-    return "I can't hear you!"
-  } else if (string === string.toUpperCase()){
-      if (string === 'I love you, Grandma') {
-        return 'I love you, too'
-      }else {
-        return "I can't hear you!"
-      }
+  if (string === 'I love you, Grandma') {
+    return "I love you, too";
+  } else if (string !== string.toUpperCase()) {
+    return "I can't hear you"
+  } else {
+    return ''
   }
 }
+
 
